d56b27232dec96f526867d48400c898d4995f51b
DamienBTech <dholt35@ivytech.edu>
2022-12-07 17:18:31 -0500

    Added EJS, Dynamic Stuff and CSS

--- app.js
+++ app.js
@@ -2,25 +2,34 @@ const { response } = require('express');
 const express = require('express');
 //app
 const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
 //listen for requests
 
 app.listen(3000);
 
-app.get('/', (req, res) =>{
-    //res.send('<p>Home Page</p>');
-    res.sendFile('./views/index.html', {root: __dirname});
-});
-app.get('/about', (req, res) =>{
-    //res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+app.get('/', (req,res)=>{
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'More placeholder text'},
+        {title: 'Mario finds stars', snippet: 'More placeholder text'},
+        {title: 'How to defeat Bowser', snippet: 'More placeholder text'},
+    ];
+    res.render('index', { title: 'Home', blogs});
 });
 
-// redirects
-app.get('/about-us', (req, res)=> {
-    res.redirect('/about');
+app.get('/about', (req, res) =>{
+    //res.send('<p>About Page</p>');
+    res.render('about', { title: 'About'})
+});
+
+
+app.get('/blogs/create', (req, res)=>{
+    res.render('create', { title: 'Create a new Blog'})
 })
 
 //404 (LAST)
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', { title: '404'});
 });
